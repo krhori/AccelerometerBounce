4986f5ee95647c09a5766761b8ec366a343f901a
krhori <khori@US-S414LAB-LAP9.local>
2013-09-30 12:20:38 -1000

    First Commit of project
    The ball bounces off the walls and is affected by gravity

---
+++ Lab 6Tests/Lab_6Tests.h
@@ -0,0 +1,13 @@
+//
+//  Lab_6Tests.h
+//  Lab 6Tests
+//
+//  Created by Kristen Hori on 9/15/13.
+//  Copyright (c) 2013 AgencyAgency. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Lab_6Tests : SenTestCase
+
+@end
---
+++ Lab 6Tests/Lab_6Tests.m
@@ -0,0 +1,32 @@
+//
+//  Lab_6Tests.m
+//  Lab 6Tests
+//
+//  Created by Kristen Hori on 9/15/13.
+//  Copyright (c) 2013 AgencyAgency. All rights reserved.
+//
+
+#import "Lab_6Tests.h"
+
+@implementation Lab_6Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Lab 6Tests");
+}
+
+@end
